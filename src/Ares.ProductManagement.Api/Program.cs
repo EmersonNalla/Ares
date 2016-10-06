d6f4fdf530b44725d7a07e49187ac20957fd81b1
EmersonNalla <nalla@emersonecologics.com>
2016-10-06 14:34:57 -0400

    New project structure

--- src/Ares.ProductManagement.Api/Program.cs
+++ src/Ares.ProductManagement.Api/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 
-namespace Ares
+namespace Ares.ProductManagement.Api
 {
     public class Program
     {
